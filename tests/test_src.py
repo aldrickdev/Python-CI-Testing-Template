0deaad841af1059566fcfbee33d0e24cfe8a283e
aldrick.castro <aldrick.castro@datadoghq.com>
2023-09-13 19:37:52 +0000

    added custom tags and metrics

--- tests/test_src.py
+++ tests/test_src.py
@@ -1,13 +1,30 @@
-from hello import SayHello
 import pytest
+from ddtrace import tracer
+from hello import SayHello
 
 
-def test_SayHello_one_fast():
-    assert SayHello("Datadog", 5) == "Hello Datadog"
+def test_SayHello_one_fast(ddspan):
+    delay = 5
+
+    # Adding custom metric
+    ddspan.set_Tag("artificial_delay", delay)
+
+    # Adding custom tags
+    ddspan.set_Tag("test_type", "one_fast")
+
+    assert SayHello("Datadog", delay) == "Hello Datadog"
 
 
-def test_SayHello_one_slow():
-    assert SayHello("Datadog", 15) == "Hello Datadog"
+def test_SayHello_one_slow(ddspan):
+    delay = 15
+
+    # Adding custom metric
+    ddspan.set_Tag("artificial_delay", delay)
+
+    # Adding custom tags
+    ddspan.set_Tag("test_type", "one_fast")
+
+    assert SayHello("Datadog", delay) == "Hello Datadog"
 
 
 @pytest.mark.parametrize(
@@ -25,7 +42,12 @@ def test_SayHello_one_slow():
         ("Datadog", 5, "Hello Datadog"),
     ],
 )
-def test_SayHello_ten_fast(name: str, delay: int, expected: str):
+def test_SayHello_ten_fast(name: str, delay: int, expected: str, ddspan):
+    # Adding custom metric
+    ddspan.set_Tag("artificial_delay", delay)
+
+    # Adding custom tags
+    ddspan.set_Tag("test_type", "ten_fast")
     assert SayHello(name, delay) == expected
 
 
@@ -44,5 +66,10 @@ def test_SayHello_ten_fast(name: str, delay: int, expected: str):
         ("Datadog", 15, "Hello Datadog"),
     ],
 )
-def test_SayHello_ten_slow(name: str, delay: int, expected: str):
+def test_SayHello_ten_slow(name: str, delay: int, expected: str, ddspan):
+    # Adding custom metric
+    ddspan.set_Tag("artificial_delay", delay)
+
+    # Adding custom tags
+    ddspan.set_Tag("test_type", "ten_slow")
     assert SayHello(name, delay) == expected
